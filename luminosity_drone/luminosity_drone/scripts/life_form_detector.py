83a17bd9003c3bb95495d03f826234a243f22589
Dante-ryder <96434775+Dante-ryder@users.noreply.github.com>
2023-11-10 14:20:02 +0530

    Update life_form_detector.py

--- luminosity_drone/luminosity_drone/scripts/life_form_detector.py
+++ luminosity_drone/luminosity_drone/scripts/life_form_detector.py
@@ -29,10 +29,20 @@ CODE MODULARITY AND TECHNIQUES MENTIONED LIKE THIS WILL HELP YOU GAINING MORE MA
 
 #Import all the required pakages
 import rospy
+import numpy as np
+from sensor_msgs.msg import Image
+from luminosity_drone.msg import Biolocation
+import cv2
+import math
 from swift_msgs.msg import *
 from std_msgs.msg import *
-from pid_tune.msg import PidTune
-import queue
+from imutils import contours
+from skimage import measure
+import numpy as np
+import imutils
+from geometry_msgs.msg import PoseArray
+from std_msgs.msg import Float64
+
 
 
 class swift:
@@ -45,8 +55,13 @@ class swift:
         self.drone_position = [0.0, 0.0, 0.0]
 
         #set points are set manually to lift the drone high and land it 
-        self.setpoint = [0.0, 0.0, 0.0]
+        self.setpoint = [0.0, 0.0, 22.0]
+        self.visitedsetpoints = [[]]
 
+# , [0, 2, 20], [0, 4, 20], [0, -2, 20], [0, -4, 20], [2, 0, 20], [2, 2, 20], 
+#                          [2, 4, 20], [2, -2, 20], [2, -4, 20], [4, 0, 20], [4, 2, 20], [4, 4, 20], [4, -2, 20], 
+#                          [4, -4, 20], [-2, 0, 20], [-2, 2, 20], [-2, 4, 20], [-2, -2, 20], [-2, -4, 20], 
+#                          [-4, 0, 20], [-4, 2, 20], [-4, 4, 20], [-4, -2, 20], [-4, -4, 20]
         #Declaring a cmd of message type swift_msgs and initializing values
         self.cmd = swift_msgs()
         self.cmd.rcRoll = 1500
@@ -60,28 +75,35 @@ class swift:
 
         #initial setting of Kp, Kd and ki for [roll, pitch, throttle]. eg: self.Kp[2] corresponds to Kp value in throttle axis
 		#after tuning and computing corresponding PID parameters, change the parameters
-        self.Kp = [5.3, 10.3, 11.179991999999199]
+        self.Kp = [7.3, 13.3, 11.179991999999199]
         self.Ki = [0, 0, 0.04800401758]
         self.Kd = [10, 20, 468.87479]
 
         #errors for pid
         self.alt_error = [0.0, 0.0, 0.0]
         self.prev_alt_error = [0.0, 0.0, 0.0]
+        self.sum_alt_error = [0.0, 0.0, 0.0]
 
         #min and max values for throttle
         self.min = 1000
         self.max = 2000
+        self.helper = 0
+        self.opt = 0
 
         # pub;ish /drone_command and errors for analytics purpose /alt_error, /pitch_error and /roll_error
         self.command_pub = rospy.Publisher('/drone_command', swift_msgs, queue_size = 1)
+        self.location_pub = rospy.Publisher('/astrobiolocation', Biolocation, queue_size = 1)
         self.alt_error_pub = rospy.Publisher('/alt_error', Float64, queue_size = 1)
         self.pitch_error_pub = rospy.Publisher('/pitch_error', Float64, queue_size = 1)
         self.roll_error_pub = rospy.Publisher('/roll_error', Float64, queue_size = 1)
 
+        self.loc = Biolocation()
+        
+
         # Subscribing to /whycon/poses, (/pid_tuning_altitude, /pid_tuning_pitch, pid_tuning_roll)=> to tune pid
         rospy.Subscriber('whycon/poses', PoseArray, self.whycon_callback)
 
-        # rospy.Subscriber('/pid_tuning_altitude',PidTune,self.altitude_set_pid)
+        # rospy.Subscriber('/swift/camera_rgb/image_raw', PoseArray,self.altitude_set_pid)
         # rospy.Subscriber('/pid_tuning_pitch',PidTune,self.pitch_set_pid)
         # rospy.Subscriber('/pid_tuning_roll',PidTune,self.roll_set_pid)
 
@@ -114,13 +136,136 @@ class swift:
 
     # pid controller for the drone
     def pid(self):
-        self.cmd.rcRoll = 1500
-        self.cmd.rcYaw = 1500
-        self.cmd.rcPitch = 1500
-        self.cmd.rcThrottle = 1590
-        self.cmd.rcAUX4 = 1500
+        if self.alt_error[0] < 0.2 and self.alt_error[0] > -0.2 and self.alt_error[1] < 0.2 and self.alt_error[1] > -0.2 and self.alt_error[2] < 0.2 and self.alt_error[2] > -0.2:
+            self.helper+=1
+            if(self.helper==5):
+                self.helper=0
+                if self.opt >=4:
+                    self.opt=0
+                rospy.Subscriber('/swift/camera_rgb/image_raw', Image, self.detect_life_form)
+                self.opt+=1  #navigate to the next setpoint
+                self.changeposition()
+                
+        # rospy.loginfo("<------####------.")
+        # rospy.loginfo("Throttle : %s",str(self.cmd.rcThrottle))
+        # rospy.loginfo("Error : %s",str(self.alt_error[2]))
+        # rospy.loginfo("Kp : %s",str(self.Kp[2]))
+        # rospy.loginfo("Ki : %s",str(self.Ki[2]))
+        # rospy.loginfo("Kd : %s",str(self.Kd[2]))
+        # rospy.loginfo("<------####------.")
 
+		# for altitude
+        self.alt_error[2] = - (self.setpoint[2] - self.drone_position[2])
+        self.cmd.rcThrottle = 1550 + int((self.alt_error[2] * self.Kp[2]) + ((self.alt_error[2] - self.prev_alt_error[2]) * self.Kd[2]) + (self.sum_alt_error[2] * self.Ki[2]))
+        if (self.cmd.rcThrottle > 2000):
+            self.cmd.rcThrottle = 2000
+        if (self.cmd.rcThrottle < 1000):
+            self.cmd.rcThrottle = 1000
+        self.pitch_error_pub.publish(self.cmd.rcThrottle)
+        self.prev_alt_error[2] = self.alt_error[2]
+        self.sum_alt_error[2] += self.alt_error[2]
+
+        # rospy.loginfo("<------####------.")
+        # rospy.loginfo("pitch : %s",str(self.cmd.rcPitch))
+        # rospy.loginfo("Error : %s",str(self.alt_error[1]))
+        # rospy.loginfo("Kp : %s",str(self.Kp[1]))
+        # rospy.loginfo("Ki : %s",str(self.Ki[1]))
+        # rospy.loginfo("Kd : %s",str(self.Kd[1]))
+        # rospy.loginfo("<------####------.")
+
+		# for pitch
+        self.alt_error[1] =  - (self.setpoint[1] - self.drone_position[1])
+        self.cmd.rcPitch = 1500 + int((self.alt_error[1] * self.Kp[1]) + ((self.alt_error[1] - self.prev_alt_error[1]) * self.Kd[1]) + (self.sum_alt_error[1] * self.Ki[1]))
+        if (self.cmd.rcPitch > 2000):
+            self.cmd.rcPitch = 2000
+        if (self.cmd.rcPitch < 1000):
+            self.cmd.rcPitch = 1000
+        self.pitch_error_pub.publish(self.cmd.rcPitch)
+        self.prev_alt_error[1] = self.alt_error[1]
+        self.sum_alt_error[1] += self.alt_error[1]
+
+        # rospy.loginfo("<------####------.")
+        # rospy.loginfo("Roll : %s",str(self.cmd.rcRoll))
+        # rospy.loginfo("Error : %s",str(self.alt_error[0]))
+        # rospy.loginfo("Kp : %s",str(self.Kp[0]))
+        # rospy.loginfo("Ki : %s",str(self.Ki[0]))
+        # rospy.loginfo("Kd : %s",str(self.Kd[0]))
+        # rospy.loginfo("<------####------.")
+
+        # for roll
+        self.alt_error[0] =  (self.setpoint[0] - self.drone_position[0])
+        self.cmd.rcRoll = 1500 + int((self.alt_error[0] * self.Kp[0]) + ((self.alt_error[0] - self.prev_alt_error[0]) * self.Kd[0]) + (self.sum_alt_error[0] * self.Ki[0]))
+        if (self.cmd.rcRoll > 2000):
+            self.cmd.rcRoll = 2000
+        if (self.cmd.rcRoll < 1000):
+            self.cmd.rcRoll = 1000
+        self.pitch_error_pub.publish(self.cmd.rcRoll)
+        self.prev_alt_error[0] = self.alt_error[0]
+        self.sum_alt_error[0] += self.alt_error[0]
         self.command_pub.publish(self.cmd)
+        self.loc.organism_type = "A"
+        self.loc.whycon_x = 0
+        self.loc.whycon_y = 1
+        self.loc.whycon_z = 0
+        self.location_pub.publish(self.loc)
+
+
+    def changeposition(self):
+        temp = []
+        for i in self.setpoint:
+            temp.append(i)
+        self.visitedsetpoints.append(self.setpoint) 
+        if self.opt ==1:
+            temp[0] += 2.25
+            if temp in self.visitedsetpoints:
+                temp[0] -= 2.25
+        elif self.opt == 2:
+            temp[1] += 2.25
+            if temp in self.visitedsetpoints:
+                temp[1] -= 2.25
+        elif self.opt == 3:
+            temp[0] -= 2.25
+            if temp in self.visitedsetpoints:
+                temp[0] += 2.25
+        elif self.opt == 4:
+            temp[1] -= 2.25
+            if temp in self.visitedsetpoints:
+                temp[1] += 2.25
+        print(f"temp : {temp}")
+        while temp in self.visitedsetpoints:
+            if self.opt <=1:
+                self.opt =5
+            self.opt -=1
+            print(self.opt)
+            if self.opt ==1:
+                temp[0] += 2.25
+                if temp in self.visitedsetpoints:
+                    temp[0] -= 2.25
+            elif self.opt == 2:
+                temp[1] += 2.25
+                if temp in self.visitedsetpoints:
+                    temp[1] -= 2.25
+            elif self.opt == 3:
+                temp[0] -= 2.25
+                if temp in self.visitedsetpoints:
+                    temp[0] += 2.25
+            elif self.opt == 4:
+                temp[1] -= 2.25
+                if temp in self.visitedsetpoints:
+                    temp[1] += 2.25
+            if temp[0] >= 6 and temp[0] <= -6 and temp[1] >= 6 and temp[1] <=-6:
+                print('hi')
+                if self.opt >=4:
+                    self.opt-=1
+                else:
+                    self.opt+=1
+            print(f"temp final : {temp}")
+        
+        self.setpoint=temp
+        
+
+    def detect_life_form(self, img):
+        pass
 
 
 
@@ -132,4 +277,6 @@ if __name__=='__main__':
     while not rospy.is_shutdown():
         swift_drone.pid()
         r.sleep()
+    print(swift_drone.visitedsetpoints)
+
     # controller ended
